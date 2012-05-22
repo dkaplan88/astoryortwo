63261f18a7d3df5e5efea5b3553136b094b2d0aa
Andrew Deitrick <ajdeitrick@gmail.com>
2012-05-22 14:34:02 -0500

    Rails Newwww

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astoryortwo::Application.config.secret_token = '0b312f87d13d38613f7d95ee8931444a83a2c56dfa8b52ab684af7a35d0589e77946d00d7d1d46fa08aa00139e9d765060fc1267e025893f46614248af6e9b5d'
