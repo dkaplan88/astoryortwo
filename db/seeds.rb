b83e22c5bebc1b66e91a9506bc4901f399afd39b
Andrew Deitrick <ajdeitrick@gmail.com>
2012-05-25 17:53:04 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,10 @@ if Rails.env.development?
   Submission.destroy_all
   User.destroy_all
   Invite.destroy_all
+  Line.destroy_all
+  
+  User.create login: "Andrew", name: "Andrew", password: "tree", password_confirmation: "tree"
+  Story.create title: "Jack & Jill"
+  Line.create content: "Jack and Jill went up a hill to fetch a pail of water", user_id: 1, story_id: 1
+  puts "All set, user login is Andrew, pw is tree"
 end
